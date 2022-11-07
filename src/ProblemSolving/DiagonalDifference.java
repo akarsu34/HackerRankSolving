9c0fe6044434be71cd3ac917341da8d8cf9e029a
düzgün akarsu <duzgunakarsu@gmail.com>
2022-11-07 19:13:56 +0300

    add DiagonalDifference and MarkAndToys

---
+++ src/ProblemSolving/DiagonalDifference.java
@@ -0,0 +1,55 @@
+package ProblemSolving;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+import java.util.stream.Stream;
+
+public class DiagonalDifference {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+
+        int n = Integer.parseInt(bufferedReader.readLine().trim());
+
+        List<List<Integer>> arr = new ArrayList<>();
+
+        IntStream.range(0, n).forEach(i -> {
+            try {
+                arr.add(
+                        Stream.of(bufferedReader.readLine().replaceAll("\\s+$", "").split(" "))
+                                .map(Integer::parseInt)
+                                .collect(Collectors.toList())
+                );
+            } catch (IOException ex) {
+                throw new RuntimeException(ex);
+            }
+        });
+
+        int result = DiagonalDifference.diagonalDifference(arr);
+
+        System.out.println("result : " + result);
+
+        bufferedReader.close();
+    }
+
+
+    public static int diagonalDifference(List<List<Integer>> arr) {
+        // Write your code here
+        int listSize = arr.get(0).size();
+        int a = 0;
+        int b = 0;
+        for (int i = 0, j = listSize - 1; i < listSize; i++, j--) {
+            a += arr.get(i).get(i);
+            b += arr.get(i).get(j);
+        }
+        int result = a - b;
+        if (result < 0) {
+            result = result * -1;
+        }
+        return result;
+    }
+}
---
+++ src/ProblemSolving/MarkAndToys.java
@@ -0,0 +1,45 @@
+package ProblemSolving;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+public class MarkAndToys {
+    public static void plusMinus(List<Integer> arr) {
+        // Write your code here
+        double negativeCount = 0;
+        double positiveCount = 0;
+        double zeroCount = 0;
+        double listSize = arr.size();
+        for (int i = 0; i < listSize; i++) {
+            if (arr.get(i) < 0) {
+                negativeCount++;
+            } else if (arr.get(i) == 0) {
+                zeroCount++;
+            } else {
+                positiveCount++;
+            }
+        }
+        System.out.print((positiveCount / listSize) + "\n" + (negativeCount / listSize) + "\n" + (zeroCount / listSize));
+
+    }
+
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+
+        int n = Integer.parseInt(bufferedReader.readLine().trim());
+
+        List<Integer> arr = Stream.of(bufferedReader.readLine().replaceAll("\\s+$", "").split(" "))
+                .map(Integer::parseInt)
+                .collect(Collectors.toList());
+
+        MarkAndToys.plusMinus(arr);
+
+        bufferedReader.close();
+    }
+}
+
